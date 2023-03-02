d12325eb7c2f432593215127a033b211ceeed3dc
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-03-02 04:12:37 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -133,19 +133,6 @@ int main() {
             glm::vec3(-1.3f,  1.0f, -1.5f)
     };
 
-    glm::vec3 cubePositions[] = {
-            glm::vec3(0.0f, 0.0f, 0.0f),
-            glm::vec3(2.0f, 5.0f, -15.0f),
-            glm::vec3(-1.5f, -2.2f, -2.5f),
-            glm::vec3(-3.8f, -2.0f, -12.3f),
-            glm::vec3(2.4f, -0.4f, -3.5f),
-            glm::vec3(-1.7f, 3.0f, -7.5f),
-            glm::vec3(1.3f, -2.0f, -2.5f),
-            glm::vec3(1.5f, 2.0f, -2.5f),
-            glm::vec3(1.5f, 0.2f, -1.5f),
-            glm::vec3(-1.3f, 1.0f, -1.5f)
-    };
-
     ///////////////////////////////////////////////////////////////////////
     // VAO, VBO Configuration                                            //
     //////////////////////////////////////////////////////////////////////
@@ -240,7 +227,6 @@ int main() {
 
         // render the cube
         glBindVertexArray(cubeVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
 
         for (int i = 0; i < 10; i++) {
             glm::mat4 model = glm::mat4(1.0f);
