3611abc69cba3510fd36d51d38d0bc72b9295afe
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-03-06 23:16:56 +0300

    refactor(Mesh): made some fields private

--- src/Mesh.h
+++ src/Mesh.h
@@ -32,18 +32,17 @@ struct Texture {
 
 class Mesh {
 public:
-    // mesh Data
-    std::vector<Vertex> vertices;
-    std::vector<unsigned int> indices;
-    std::vector<Texture> textures;
-    unsigned int VAO{};
-
     Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
 
     // render the mesh
     void Draw(Shader& shader);
 
 private:
+    // mesh Data
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+    unsigned int VAO{};
     // render data
     unsigned int VBO{}, EBO{};
 
