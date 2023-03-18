eca9ce1cbc5b7161026e4e03e34cd4423f07f0e8
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-03-18 06:43:21 +0300

    feat(Cmake): added imgui

--- src/Input.cpp
+++ src/Input.cpp
@@ -2,6 +2,7 @@
 #include <SDL2/SDL.h>
 #include "Configs.hpp"
 #include "Camera.h"
+#include "../libs/imgui/imgui_impl_sdl.h"
 
 
 Input::Input() :
@@ -11,7 +12,17 @@ Input::Input() :
 
 
 void Input::Process(Camera& camera, float dt, bool& isRunning) {
-    SDL_PumpEvents();
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    ImGui_ImplSDL2_ProcessEvent(&event);
+    ImGuiIO& io = ImGui::GetIO();
+
+    int mouseX, mouseY;
+    const int buttons = SDL_GetMouseState(&mouseX, &mouseY);
+    io.MousePos = ImVec2(mouseX, mouseY);
+    io.MouseDown[0] = buttons & SDL_BUTTON(SDL_BUTTON_LEFT);
+    io.MouseDown[1] = buttons & SDL_BUTTON(SDL_BUTTON_RIGHT);
 
     ProcessKeyboard(camera, dt, isRunning);
     ProcessMouse(camera);
@@ -38,7 +49,7 @@ void Input::ProcessKeyboard(Camera& camera, float dt, bool& isRunning) {
 void Input::ProcessMouse(Camera& camera) {
     SDL_GetMouseState(&mouseX, &mouseY);
     CalculateMouseOffset();
-    camera.ProcessMouseMovement(mouseOffsetX, mouseOffsetY);
+    //camera.ProcessMouseMovement(mouseOffsetX, mouseOffsetY);
 }
 
 
