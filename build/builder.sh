4f99e6ceda9c627ee660474e926235238793647d
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-11-09 00:02:56 +0300

    build(sh): added builder.sh

---
+++ build/builder.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p build_temp && cd build_temp || exit
+
+OS="$(uname -s)"
+if [ "$OS" = "Linux" ]; then
+    NPROCS="$(nproc --all)"
+elif [ "$OS" = "Darwin" ] || [ "$(echo "$OS" | grep -q BSD)" = "BSD" ]; then
+    NPROCS="$(sysctl -n hw.ncpu)"
+else
+    NPROCS="$(getconf _NPROCESSORS_ONLN)"
+fi
+
+cmake -DCMAKE_BUILD_TYPE=Release ../.. && cmake --build . --parallel "$NPROCS"
