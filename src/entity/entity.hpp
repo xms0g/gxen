63d685e42fc24cf1c9825a33b5170cd105645d8f
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-09-21 17:53:22 +0300

    refactor(entity): convert `Entity` from struct to class for consistency

--- src/entity/entity.hpp
+++ src/entity/entity.hpp
@@ -3,12 +3,13 @@
 #include "glm/glm.hpp"
 
 class Shader;
-struct Entity {
+class Entity {
+public:
+	virtual ~Entity() = default;
+	[[nodiscard]] virtual Shader* shader() const = 0;
+	virtual void draw() const = 0;
+
     float rotation{0.0f};
     float scale{1.0f};
     glm::vec3 position{0.0f};
-
-    virtual ~Entity() = default;
-    [[nodiscard]] virtual Shader* shader() const = 0;
-    virtual void draw() const = 0;
 };
