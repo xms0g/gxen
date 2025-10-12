65f0f3bebd9b92bde25f5432796996f47251aef3
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-10-12 18:01:41 +0300

    Revert "refactor(shadow rendering): enhance ShadowData structure and update shadow handling logic"
    This reverts commit 0c7691eb7df2f0e94e48eed1518aafd0a81d4025.

--- src/rendering/shadowPass/shadowData.hpp
+++ src/rendering/shadowPass/shadowData.hpp
@@ -3,28 +3,8 @@
 #include <glm/glm.hpp>
 
 struct ShadowData {
-	struct IShadow {
-		uint32_t shadowMap;
-	};
-
-	struct DirShadow : IShadow {
-		glm::mat4 lightSpaceMatrix;
-
-		DirShadow(const uint32_t shadowMap, const glm::mat4& lightSpaceMatrix) :
-			IShadow(shadowMap),
-			lightSpaceMatrix(lightSpaceMatrix) {
-		}
-	};
-
-	struct PointShadow : IShadow {
-		float farPlane;
-
-		PointShadow(const uint32_t shadowMap, const float farPlane) :
-			IShadow(shadowMap),
-			farPlane(farPlane) {
-		}
-	};
-
-	std::vector<DirShadow> dirShadows;
-	std::vector<PointShadow> pointShadows;
+	uint32_t shadowMap;
+	uint32_t shadowCubemap;
+	glm::mat4 lightSpaceMatrix;
+	float omniFarPlane;
 };
